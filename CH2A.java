d081e0e595926b4b56f55ab998b18c51d6da2c79
hrkuiper <hrkuiper@students.unwsp.edu>
2022-09-01 18:24:31 -0500

    MY super cool in-class project

---
+++ CH2A.java
@@ -0,0 +1,22 @@
+package inclass;
+
+public class CH2A {
+	
+		public static void main(String[] args)
+		{
+		int	Student1 = 55;
+		int	Student2 = 66;
+		int	Student3 = 89;
+		int	Student4 = 72;
+		int	Student5 = 32;
+		
+	int	avg = Student1 + Student2 + Student3 + Student4 + Student5;
+	
+	int answer = avg / 5; 
+	
+	System.out.println(" The average height is " + answer);
+	
+						
+		}
+}
+		
